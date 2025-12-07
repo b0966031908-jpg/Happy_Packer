3e7fe94c7e800996e99423bca67fc531ada8e413
b0966031908-jpg <b0966031908@gmail.com>
2025-12-07 14:31:19 +0800

    V.2新增其餘功能的資料夾

--- app/src/main/java/com/b0966031908gmail/happypacker/ui/home/HomeFragment.kt
+++ app/src/main/java/com/b0966031908gmail/happypacker/ui/home/HomeFragment.kt
@@ -1,15 +1,13 @@
-package com.yourpackage.happypacker.ui.home
+package com.b0966031908gmail.happypacker.ui.home
 
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.fragment.app.Fragment
-import androidx.fragment.app.viewModels
 import androidx.lifecycle.lifecycleScope
-import androidx.navigation.fragment.findNavController
-import com.yourpackage.happypacker.R
-import com.yourpackage.happypacker.databinding.FragmentHomeBinding
+import com.yourpackage.happypacker.ui.home.HomeButton
+import com.yourpackage.happypacker.ui.home.HomeUiState
+import com.yourpackage.happypacker.ui.home.HomeViewModel
 import kotlinx.coroutines.launch
 
 class HomeFragment : Fragment() {
---
+++ app/src/main/java/com/b0966031908gmail/happypacker/ui/home/HomeViewModel.kt
@@ -0,0 +1,26 @@
+package com.b0966031908gmail.happypacker
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupActionBarWithNavController
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val navHostFragment = supportFragmentManager
+            .findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        val navController = navHostFragment.navController
+
+        setupActionBarWithNavController(navController)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        val navHostFragment = supportFragmentManager
+            .findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        return navHostFragment.navController.navigateUp() || super.onSupportNavigateUp()
+    }
+}
